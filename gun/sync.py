13ce02d7a9a3604dddc11649898d2922cb138489
Andriy Yurchuk <ch00k@colossal.(none)>
2012-03-09 20:24:57 +0200

    Added 'eix-remote update' task. Cleaned up code

--- gun/sync.py
+++ gun/sync.py
@@ -21,15 +21,15 @@ import logging as log
 import sys
 
 
-"""Setup logging"""
+# Setup logging
 log.basicConfig(filename = '/var/log/gun.log',
                 format='[%(asctime)s]: %(levelname)s: %(message)s',
                 datefmt = '%Y-%m-%d, %H:%M:%S',
                 level = log.DEBUG)
 
-"""Reading all the configuration"""
+# Reading all the configuration
 config = ConfigParser.RawConfigParser()
-if config.read('/etc/gun/gun.conf') != []:
+if config.read('/etc/gun/gun.conf'):
     pass
 else:
     log.critical('Cannot open configuration file')
@@ -37,11 +37,15 @@ else:
 
 try:
     SYNC_OVERLAYS = config.getboolean('GENERAL', 'sync_overlays')
+    SYNC_EIX_REMOTE = config.getboolean('GENERAL', 'sync_eix_remote')
     SYNC_TREE_COMMAND = config.get('SYNC', 'tree_command')
     SYNC_TREE_OPTIONS = config.get('SYNC', 'tree_options')
     if SYNC_OVERLAYS:
         SYNC_OVERLAYS_COMMAND = config.get('SYNC', 'overlays_command')
         SYNC_OVERLAYS_OPTIONS = config.get('SYNC', 'overlays_options')
+    if SYNC_EIX_REMOTE:
+        SYNC_EIX_REMOTE_COMMAND = config.get('SYNC', 'eix_remote_command')
+        SYNC_EIX_REMOTE_OPTIONS = config.get('SYNC', 'eix_remote_options')
     EMERGE_COMMAND = config.get('UPDATE', 'command')
     EMERGE_OPTIONS = config.get('UPDATE', 'options')
     EMAIL_NOTIFY = config.getboolean('NOTIFY', 'email')
@@ -64,26 +68,24 @@ try:
     JABBER_PASSWORD = config.get('JABBER', 'password')
     JABBER_RECIPIENT = config.get('JABBER', 'jabberto')
 except ConfigParser.NoSectionError, error:
-    log.critical('Errors reading config file: %s' % (error))
-    sys.exit('Errors reading config file: %s' % (error))
+    log.critical('Errors reading config file: %s' % error)
+    sys.exit('Errors reading config file: %s' % error)
 except ConfigParser.NoOptionError, error:
-    log.critical('Errors reading config file: %s' % (error))
-    sys.exit('Errors reading config file: %s' % (error))
+    log.critical('Errors reading config file: %s' % error)
+    sys.exit('Errors reading config file: %s' % error)
 
 timestamp = time.strftime('%Y%m%d-%H%M%S')
-"""Creating tempfile"""
+# Creating tempfile
 try:
-    OUTPUT_FILE = tempfile.NamedTemporaryFile(suffix = '',
-                                              prefix = 'gun-',
-                                              dir = '/var/tmp',
-                                              delete = True)
+    OUTPUT_FILE = tempfile.NamedTemporaryFile(prefix = 'gun-',
+                                              dir = '/var/tmp')
 except OSError, error:
-    log.critical('Cannot create tempfile: %s' % (error))
-    sys.exit('Cannot create tempfile: %s' % (error))
+    log.critical('Cannot create tempfile: %s' % error)
+    sys.exit('Cannot create tempfile: %s' % error)
     
 
-"""A replacement dict, that maps ANSI escape codes to HTML tags to use
-within the formatter function"""
+# A replacement dict, that maps ANSI escape codes to HTML tags to use
+# within the formatter function
 ESCAPE_MAP = \
 {
     '\x1b[32m': '</span><span style="color:darkgreen;font-weight:normal">',
@@ -121,26 +123,34 @@ class Gun(object):
         """Sync Portage tree and overlays
         """
         command = '%s %s' % (SYNC_TREE_COMMAND, SYNC_TREE_OPTIONS)
-        log.info('Running sync tree command: "%s"' % (command))
+        log.info('Running sync tree command: "%s"' % command)
         self._execute_command(command = command,
                               stdout = subprocess.PIPE)
         
         if SYNC_OVERLAYS:
             command = '%s %s' % (SYNC_OVERLAYS_COMMAND, SYNC_OVERLAYS_OPTIONS)
-            log.info('Running sync overlays command: "%s"' % (command))
+            log.info('Running sync overlays command: "%s"' % command)
             self._execute_command(command = command,
                                   stdout = subprocess.PIPE)
         else:
             log.info('Sync overlays not requested. Skipping.')
+
+        if SYNC_EIX_REMOTE:
+            command = '%s %s' % (SYNC_EIX_REMOTE_COMMAND, SYNC_EIX_REMOTE_OPTIONS)
+            log.info('Running sync eix remote database command: "%s"' % command)
+            self._execute_command(command = command,
+                stdout = subprocess.PIPE)
+        else:
+            log.info('Sync remote overlays database not requested. Skipping.')
 
     def pretend_update(self):
         """Runs the emerge command to get the list of packages ready for update.
         Writes the list to file.
         """
         command = '%s %s --pretend --color y' % (EMERGE_COMMAND, EMERGE_OPTIONS)
-        log.info('Running emerge: "%s"' % (command))
-        output = self._execute_command(command = command,
-                                       stdout = OUTPUT_FILE)
+        log.info('Running emerge: "%s"' % command)
+        self._execute_command(command = command,
+                              stdout = OUTPUT_FILE)
         OUTPUT_FILE.seek(0)
         
     def notify(self):
@@ -154,8 +164,8 @@ class Gun(object):
                                                 password = MAIL_PASSWORD)
                 self.notifiers.append(email_notifier)
                 log.info('Enabling Email notification')
-            except error:
-                log.error('Email notification disabled: %s' % error)
+            except:
+                log.error('Email notification disabled')
                 pass
         if JABBER_NOTIFY:
             try:
@@ -167,7 +177,7 @@ class Gun(object):
             except:
                 log.error('Jabber notification disabled')
                 pass
-        if self.notifiers == []:
+        if not self.notifiers:
             log.warning('No notification methods available. Nothing to do...')
         for notifier in self.notifiers:
             notifier.send()
@@ -181,7 +191,7 @@ class Message(object):
         try:
             self.text = input_file.read()
         except OSError, error:
-            log.error('Cannot open output file for formatting: %s' % (error))
+            log.error('Cannot open output file for formatting: %s' % error)
         input_file.seek(0)
             
     def formatter(self, escape_map):
@@ -240,9 +250,9 @@ class EmailNotifier(object):
             else:
                 raise ValueError('Invalid SMTP server port')
         except socket.error, error:
-            log.error('Cannot connect to SMTP server: %s: '
-                      % (error,
-                         server))
+            log.error('Cannot connect to SMTP server \'%s\': %s'
+                      % (server,
+                         error))
             raise
         except ValueError, error:
             log.error('Cannot connect to SMTP server: %s: %s'
@@ -266,9 +276,9 @@ class EmailNotifier(object):
     def send(self):
         """Compiles an email message and sends it
         """
-        headers = ('From: %s\r\n' \
-                   'To: %s\r\n' \
-                   'Subject: [%s] %s Packages to update\r\n' \
+        headers = ('From: %s\r\n'
+                   'To: %s\r\n'
+                   'Subject: [%s] %s Packages to update\r\n'
                    'Content-Type: text/html; charset=utf-8\r\n\r\n'
                    % (MAIL_SENDER,
                       MAIL_RECIPIENT,
@@ -291,7 +301,7 @@ class EmailNotifier(object):
                                to_addrs = MAIL_RECIPIENT,
                                msg = message)
         except smtplib.SMTPRecipientsRefused, error:
-            log.error('Cannot send email: %s' % (error))
+            log.error('Cannot send email: %s' % error)
         
     def disconnect(self):
         """Disconnects from SMTP server
